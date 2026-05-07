850a1784a80d02f4c789b863d7080fa491eba58d
mariovagomarzal <mariovagomarzal@gmail.com>
2026-05-07 14:46:47 +0200

    ci: build and deploy the website to GitHub Pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,3 +1,6 @@
 import {defineConfig} from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://mariovagomarzal.github.io",
+  base: "/higher_category_theory_thesis",
+});
